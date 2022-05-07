32d710d259f9bb5204a1a3c013585c7d372b4169
tuhuynh99 <hctu.study@gmail.com>
2022-05-07 09:55:33 +0700

    extract ProfileField widget

--- lib/views/profile/profile_page.dart
+++ lib/views/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'change_pass_page.dart';
 // import 'package:flutter/cupertino.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,13 +10,17 @@ class ProfilePage extends StatefulWidget {
 
 class MapScreenState extends State<ProfilePage>
     with SingleTickerProviderStateMixin {
-  bool _status = false;
-  final FocusNode myFocusNode = FocusNode();
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var mobileController = TextEditingController();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    nameController.text = 'User Name';
+    emailController.text = 'mail@gmail.com';
+    mobileController.text = '12345677';
   }
 
   @override
@@ -23,7 +29,7 @@ class MapScreenState extends State<ProfilePage>
       data: ThemeData.light(),
       child: Scaffold(
           body: Container(
-        color: Color.fromARGB(255, 250, 248, 248),
+        color: Colors.amber,
         child: ListView(
           children: <Widget>[
             Column(
@@ -112,194 +118,42 @@ class MapScreenState extends State<ProfilePage>
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
-                        Padding(
+                        const Padding(
                             padding: EdgeInsets.only(
                                 left: 25.0, right: 25.0, top: 25.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: const <Widget>[
-                                    Text(
-                                      'Personal Information',
-                                      style: TextStyle(
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    _status ? _getEditIcon() : new Container(),
-                                  ],
-                                )
-                              ],
+                            child: Text(
+                              'Personal Information',
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.bold),
                             )),
+                        ProfileFieldLabel(label: 'Name'),
+                        ProfileField(controller: nameController),
+                        ProfileFieldLabel(label: 'Email'),
+                        ProfileField(controller: emailController),
+                        ProfileFieldLabel(label: 'Mobile'),
+                        ProfileField(controller: mobileController),
                         Padding(
                             padding: const EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 25.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: const <Widget>[
-                                    Text(
-                                      'Name',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
+                                left: 25.0, right: 25.0, top: 14.0),
+                            child: TextButton(
+                                style: TextButton.styleFrom(
+                                  padding: const EdgeInsets.all(0.0),
                                 ),
-                              ],
-                            )),
-                        Padding(
-                            padding: EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 2.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Flexible(
-                                  child: TextButton(
-                                    style: TextButton.styleFrom(
-                                      minimumSize: Size.zero,
-                                      padding: EdgeInsets.zero,
-                                      tapTargetSize:
-                                          MaterialTapTargetSize.shrinkWrap,
-                                    ),
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                          hintText: "Enter Your Name",
-                                          fillColor: Colors.amber,
-                                          filled: true),
-                                      enabled: !_status,
-                                      autofocus: !_status,
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        _status = !_status;
-                                      });
-                                    },
-                                  ),
+                                child: const Text(
+                                  'Change Password',
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
                                 ),
-                              ],
-                            )),
-                        Padding(
-                            padding: EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 25.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: const <Widget>[
-                                    Text(
-                                      'Email ID',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 2.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Flexible(
-                                  child: TextFormField(
-                                    decoration: const InputDecoration(
-                                        hintText: "Enter Your Name",
-                                        fillColor: Colors.amber,
-                                        filled: true),
-                                    enabled: !_status,
-                                    autofocus: !_status,
-                                  ),
-                                )
-                              ],
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 25.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: const <Widget>[
-                                    Text(
-                                      'Mobile',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 2.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Flexible(
-                                    child: TextField(
-                                  decoration:
-                                      const InputDecoration(hintText: "0912"),
-                                  enabled: !_status,
-                                  obscureText: true,
-                                )),
-                              ],
-                            )),
-                        Padding(
-                            padding: EdgeInsets.only(
-                                left: 25.0, right: 25.0, top: 25.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: const <Widget>[
-                                    Text(
-                                      'Change Password',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )),
-                        // Padding(
-                        //     padding: const EdgeInsets.only(
-                        //         left: 25.0, right: 25.0, top: 2.0),
-                        //     child: Row(
-                        //       mainAxisSize: MainAxisSize.max,
-                        //       children: <Widget>[
-                        //         Flexible(
-                        //             child: TextField(
-                        //           decoration: const InputDecoration(
-                        //               hintText: "Password"),
-                        //           enabled: !_status,
-                        //           obscureText: true,
-                        //         )),
-                        //       ],
-                        //     )),
-                        !_status ? _getActionButtons() : new Container(),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            Change_Pass_Page()),
+                                  );
+                                })),
                       ],
                     ),
                   ),
@@ -311,82 +165,60 @@ class MapScreenState extends State<ProfilePage>
       )),
     );
   }
+}
+
+class ProfileField extends StatelessWidget {
+  ProfileField({Key? key, required this.controller}) : super(key: key);
+
+  TextEditingController controller;
 
   @override
-  void dispose() {
-    // Clean up the controller when the Widget is disposed
-    myFocusNode.dispose();
-    super.dispose();
-  }
-
-  Widget _getActionButtons() {
+  Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 45.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(right: 10.0),
-              child: Container(
-                  child: RaisedButton(
-                child: new Text("Save"),
-                textColor: Colors.white,
-                color: Colors.green,
-                onPressed: () {
-                  setState(() {
-                    _status = true;
-                    FocusScope.of(context).requestFocus(new FocusNode());
-                  });
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(20.0)),
-              )),
-            ),
-            flex: 2,
+        padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 2.0),
+        child: TextButton(
+          style: TextButton.styleFrom(
+            minimumSize: Size.zero,
+            padding: EdgeInsets.zero,
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
           ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: FlatButton(
-                child: new Text("Cancel"),
-                textColor: Colors.white,
-                color: Colors.red,
-                onPressed: () {
-                  setState(() {
-                    _status = true;
-                    FocusScope.of(context).requestFocus();
-                  });
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-              ),
-            ),
-            flex: 2,
-          ),
-        ],
-      ),
-    );
+          child: TextFormField(
+              autofocus: false,
+              controller: controller,
+              decoration: InputDecoration(
+                  fillColor: Colors.amber,
+                  contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  )
+                  // border: OutlineInputBorder(
+                  //     borderRadius:
+                  //         BorderRadius.circular(32.0))
+                  ),
+              enabled: true,
+              onEditingComplete: () {
+                FocusScope.of(context).unfocus();
+              }),
+          onPressed: () {},
+        ));
   }
+}
 
-  Widget _getEditIcon() {
-    return GestureDetector(
-      child: const CircleAvatar(
-        backgroundColor: Colors.red,
-        radius: 14.0,
-        child: Icon(
-          Icons.edit,
-          color: Colors.white,
-          size: 16.0,
-        ),
-      ),
-      onTap: () {
-        // change to the edit page
-        setState(() {
-          _status = false;
-        });
-      },
-    );
+class ProfileFieldLabel extends StatelessWidget {
+  String label;
+  ProfileFieldLabel({Key? key, required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ));
   }
 }
