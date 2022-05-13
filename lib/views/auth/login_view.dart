09f187d798a0b571007e8391e58c263b7c61d522
ngtri2081 <16164@student.vgu.edu.vn>
2022-05-14 02:20:16 +0700

    Add SHA1 key, google sign in

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -3,6 +3,13 @@ import 'package:etoet/services/auth/auth_exceptions.dart';
 import 'package:etoet/services/auth/auth_service.dart';
 import 'package:etoet/views/auth/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: <String>[
+    'email',
+  ],
+);
 
 class LoginView extends StatefulWidget {
   const LoginView({
@@ -16,6 +23,7 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+  GoogleSignInAccount? _currentUser;
 
   @override
   Widget build(BuildContext context) {
@@ -87,12 +95,18 @@ class _LoginViewState extends State<LoginView> {
               },
               child: const Text('Not registered yet ? Sign Up')),
           TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  recoverAccountRoute,
-                );
-              },
-              child: const Text('Forgot your Password?')),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                recoverAccountRoute,
+              );
+            },
+            child: const Text('Forgot your Password?'),
+          ),
+          _Button(
+              color: Colors.green,
+              image: const AssetImage('assets/images/google_logo.png'),
+              text: 'Login with Google',
+              onPressed: _handleSignIn)
         ],
       ),
     );
@@ -110,5 +124,82 @@ class _LoginViewState extends State<LoginView> {
     _email = TextEditingController();
     _password = TextEditingController();
     super.initState();
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      _googleSignIn.onCurrentUserChanged.listen((account) {
+        setState(() {
+          _currentUser = account;
+        });
+      });
+      await _googleSignIn.signIn();
+      _currentUser = _googleSignIn.currentUser;
+      if (_currentUser != null) {
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          mainRoute,
+          (route) => false,
+        );
+      }
+    } on UserNotFoundAuthException {
+      await showErrorDialog(context, 'User not found');
+    } on WrongPasswordAuthException {
+      await showErrorDialog(context, 'Incorrect password');
+    } on GenericAuthException {
+      await showErrorDialog(context, 'Error');
+    }
+  }
+}
+
+class _Button extends StatelessWidget {
+  final Color color;
+  final ImageProvider image;
+  final String text;
+  final VoidCallback onPressed;
+
+  const _Button({
+    required this.color,
+    required this.image,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+      child: GestureDetector(
+        onTap: () {
+          onPressed();
+        },
+        child: Container(
+          height: 55,
+          decoration: BoxDecoration(
+            border: Border.all(color: color),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              const SizedBox(width: 5),
+              Image(
+                image: image,
+                width: 25,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(text, style: TextStyle(color: color, fontSize: 18)),
+                    const SizedBox(width: 35),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
