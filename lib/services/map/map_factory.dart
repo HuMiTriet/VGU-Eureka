61de85ca30878742e39f6e8d6e989d85826c8890
Tam Thai <hoangtam3062002@gmail.com>
2022-05-13 19:44:35 +0700

    Change to factory method

--- lib/services/map/map_factory.dart
+++ lib/services/map/map_factory.dart
@@ -2,8 +2,17 @@ import 'package:etoet/services/map/google_map.dart';
 import 'package:flutter/material.dart';
 
 abstract class Map extends Widget {
+  factory Map(String type) {
+    if (type == 'GoogleMap') {
+      return GoogleMapImpl();
+    }
+    throw 'Can\'t create $type.';
+  }
+
   /// The address of the current map view.
   late String address;
+
+  void setContext(BuildContext context);
 
   /// Move the camera to current location of user.
   void moveToCurrentLocation();
@@ -16,54 +25,4 @@ abstract class Map extends Widget {
 
   /// Initialize the map.
   void initializeMap();
-}
-
-class MapDefault extends Map {
-  @override
-  String address = 'Address';
-
-  @override
-  Element createElement() {
-    // TODO: implement createElement
-    throw UnimplementedError();
-  }
-
-  @override
-  void initializeMap() {
-    // TODO: implement initializeMap
-  }
-
-  @override
-  void moveToCurrentLocation() {
-    // TODO: implement moveToCurrentLocation
-  }
-
-  @override
-  void updateCurrentMapAddress() {
-    // TODO: implement updateCurrentMapAddress
-  }
-
-  @override
-  void updateScreenSize(num width, num height) {
-    // TODO: implement updateScreenSize
-  }
-}
-
-class MapFactory {
-  Map getMap(String type) {
-    Map map;
-    switch (type) {
-      case 'GoogleMap':
-        {
-          map = GoogleMapImpl();
-          break;
-        }
-      default:
-        {
-          map = MapDefault();
-          break;
-        }
-    }
-    return map;
-  }
 }
