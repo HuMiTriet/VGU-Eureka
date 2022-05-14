2a6fd0b287d4bb892b30c5a78d69f8e617320eec
Triet Huynh <triet403@gmail.com>
2022-05-14 14:18:37 +0700

    updated test

--- test/auth_user_test.dart
+++ test/auth_user_test.dart
@@ -1,15 +1,24 @@
 import 'package:etoet/services/auth/auth_service.dart';
+import 'package:etoet/services/auth/auth_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('test auth_user', () async {
-    var user = await AuthService.firebase().createUser(
+    var user = AuthUser(
+      uid: '123',
       email: 'triet403@gmail.com',
-      password: 'triet403',
+      displayName: 'Triet',
+      phoneNumber: '0989898989',
+      isEmailVerified: true,
     );
 
     user.location.latitude = 10;
     user.location.longitude = 10;
+
+    user.friendUIDs.add('123');
+    user.friendUIDs.add('456');
+
+    user.friendUIDs.forEach((uid) => print(uid));
 
     expect(user.location.longitude, 10);
   });
