a5f297cd5bfc800d59543510de7efb252fc24537
tuhuynh99 <hctu.study@gmail.com>
2022-05-15 12:52:28 +0700

    using AuthUser for user profile

--- lib/views/profile/profile_page.dart
+++ lib/views/profile/profile_page.dart
@@ -1,7 +1,5 @@
 import 'package:etoet/views/profile/Widgets/edit_image_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
 import '../../services/auth/auth_user.dart';
 import 'change_pass_page.dart';
 
