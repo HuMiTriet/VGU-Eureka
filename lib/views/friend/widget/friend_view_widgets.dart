a7ff5c36e847aa7928824672e5d1c9e322560f4e
Đinh Minh Hoàng <hoangdsieud@gmail.com>
2022-05-19 13:11:30 +0700

    Change placeholder friend avatar

--- lib/views/friend/widget/friend_view_widgets.dart
+++ lib/views/friend/widget/friend_view_widgets.dart
@@ -11,7 +11,7 @@ class FriendItem extends StatelessWidget {
       elevation: 3,
       child: ListTile(
         leading: CircleAvatar(
-          backgroundImage: NetworkImage('url'),
+          backgroundImage: NetworkImage('https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d'),
         ),
         title: Text('Friend Name'),
         subtitle: Text('\$ multual friends'),
