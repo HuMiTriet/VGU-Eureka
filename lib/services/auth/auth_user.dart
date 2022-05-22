d1457e58ccdfa109abc05acb7f7f81d1e8626d1a
Triet Huynh <triet403@gmail.com>
2022-05-22 21:40:22 +0700

    changed added UserInfo parent class to AuthUser

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,35 +1,29 @@
 import 'package:etoet/services/auth/location.dart';
+import 'package:etoet/services/auth/user_info.dart';
 import 'package:flutter/foundation.dart';
 
 // This annotation immutable tells the compiler that the content of this class
 // and its subclasses will not change.
 @immutable
-class AuthUser {
+class AuthUser extends UserInfo {
   //// provide current location of the user
   final Location location = Location();
 
   final Set<String> friendUIDs = {};
 
-  final String uid;
   final bool isEmailVerified;
 
-  final String? phoneNumber;
-  final String? email;
-  final String? displayName;
-  final String? photoURL;
-
-  AuthUser({ required this.isEmailVerified,
-    required this.uid,
-    required this.email,
-    this.phoneNumber,
-    this.displayName,
-    this.photoURL,
+  AuthUser({
+    required this.isEmailVerified,
+    required super.uid,
+    required super.email,
+    super.phoneNumber,
+    super.displayName,
+    super.photoURL,
   });
 
-
-    
   @override
-    String toString() {
+  String toString() {
     return '''
       AuthUser {
         uid: $uid,
@@ -39,5 +33,5 @@ class AuthUser {
         displayName: $displayName,
       }
     ''';
-    }
+  }
 }
---
+++ lib/services/auth/user_info.dart
@@ -0,0 +1,16 @@
+class UserInfo {
+  final String uid;
+  final String email;
+
+  final String? phoneNumber;
+  final String? displayName;
+  final String? photoURL;
+
+  UserInfo({
+    required this.uid,
+    required this.email,
+    this.phoneNumber,
+    this.displayName,
+    this.photoURL,
+  });
+}
