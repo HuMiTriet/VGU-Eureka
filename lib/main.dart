1d6db9d99ebd5f60a65427931a866c482edfebf6
Triet Huynh <triet403@gmail.com>
2022-05-24 00:12:03 +0700

    remove verify email View route in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,9 @@ void main() {
         routes: {
           loginRoute: (context) => const LoginView(),
           registerRoute: (context) => const RegisterView(),
-          verifyEmailRoute: (context) => const VerifyEmailView(),
           recoverAccountRoute: (context) => const RecoverAccountView(),
           settingsRoute: (context) => const SettingsView(),
-          profileRoute: (context) => ProfilePage(),
+          profileRoute: (context) => const ProfilePage(),
         },
       ),
     ),
