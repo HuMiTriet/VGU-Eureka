9584af0a05967a28d611568906975f1df857f422
Triet Huynh <triet403@gmail.com>
2022-05-26 16:14:22 +0700

    remove started code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,11 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+
 admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-
-// export const helloWorld = functions.region("asia-southeast1").
-//     https.onRequest((request, response) => {
-//       functions.logger.info("Hello logs!", {structuredData: true});
-//       response.send("Hello from Firebase!");
-//     });
-
 export const sendPrivateNotification = functions.region("asia-southeast1").
     firestore.document("/emergencies/{userId}").onCreate((snap) => {
       const payload = {
