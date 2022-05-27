83b9907948dc270e4a197bc356bd5a33925481b8
Tam Thai <hoangtam3062002@gmail.com>
2022-05-27 21:37:52 +0700

    Add geocoding documentation, restructure class

--- lib/services/map/geocoding.dart
+++ lib/services/map/geocoding.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: omit_local_variable_types
+
 import 'dart:developer' as devtools show log;
 
 import 'package:geocoding/geocoding.dart';
@@ -7,62 +9,47 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class Geocoding {
   Geocoding._();
 
+  /// Get address from location
+  static Future<String> getAddress(LatLng location) async {
+    var placemark = await _getPlacemark(location);
+    return _getAddressWithPriority(placemark);
+  }
+
+  /// Is used to get the address from the [placemark] with priority from: city > administrativeArea > subAdministrativeArea > thoroughfare > subThoroughfare > subLocality > country.
+  static String _getAddressWithPriority(Placemark placemark) {
+    var address = 'Unknown';
+
+    return placemark.locality ??
+        placemark.administrativeArea ??
+        placemark.subAdministrativeArea ??
+        placemark.thoroughfare ??
+        placemark.subThoroughfare ??
+        placemark.subLocality ??
+        placemark.country ??
+        address;
+  }
+
   /// Get placemark from location using geocoding package.
-  static Future<String> getGeocoding(LatLng location) async {
-    var addressList = <String>['', '', '', '', '', '', '', ''];
+  static Future<Placemark> _getPlacemark(LatLng location) async {
     try {
       var listPlacemark =
           await placemarkFromCoordinates(location.latitude, location.longitude);
-      var placemarkAddress = listPlacemark.elementAt(0);
-      var country = placemarkAddress.country;
-      var city = placemarkAddress.locality;
-      var street = placemarkAddress.thoroughfare;
-      var subLocality = placemarkAddress.subLocality;
-      var postalCode = placemarkAddress.postalCode;
-      var administrativeArea = placemarkAddress.administrativeArea;
-      var subAdministrativeArea = placemarkAddress.subAdministrativeArea;
-      var subThoroughfare = placemarkAddress.subThoroughfare;
-      addressList[0] = '$country';
-      addressList[1] = '$city';
-      addressList[2] = '$street';
-      addressList[3] = '$subLocality';
-      addressList[4] = '$postalCode';
-      addressList[5] = '$administrativeArea';
-      addressList[6] = '$subAdministrativeArea';
-      addressList[7] = '$subThoroughfare';
-      devtools.log('geocoding of location: $addressList',
+      var placemark = listPlacemark.elementAt(0);
+      var country = placemark.country;
+      var city = placemark.locality;
+      var street = placemark.thoroughfare;
+      var subLocality = placemark.subLocality;
+      var postalCode = placemark.postalCode;
+      var administrativeArea = placemark.administrativeArea;
+      var subAdministrativeArea = placemark.subAdministrativeArea;
+      var subThoroughfare = placemark.subThoroughfare;
+      devtools.log(
+          'Placemark: country: $country, city: $city, administrativeArea: $administrativeArea, subAdministrativeArea: $subAdministrativeArea, street: $street, subLocality: $subLocality, subThoroughfare: $subThoroughfare, postalCode: $postalCode,',
           name: 'Geocoding: getGeocoding');
-      return _getAddress(addressList);
+      return placemark;
     } catch (e) {
       devtools.log('get Geocoding error: $e', name: 'Geocoding: getGeocoding');
-      return 'Unknown';
+      return Placemark();
     }
-  }
-
-  /// [_getAddress] is used to get the address from the [addressList with priority from: city > administrativeArea > subAdministrativeArea > subThoroughfare > country.
-  static String _getAddress(List<String> addressList) {
-    var address = 'Unknown';
-    // city
-    if (addressList[1] != '') {
-      address = addressList[1];
-
-      // administrativeArea
-    } else if (addressList[5] != '') {
-      address = addressList[5];
-
-      // subAdministrativeArea
-    } else if (addressList[6] != '') {
-      address = addressList[6];
-
-      // subThoroughfare
-    } else if (addressList[7] != '') {
-      address = addressList[7];
-
-      // country
-    } else if (addressList[0] != '') {
-      address = addressList[0];
-    }
-    devtools.log('get address: $address', name: 'Geocoding: _getAddress');
-    return address;
   }
 }
