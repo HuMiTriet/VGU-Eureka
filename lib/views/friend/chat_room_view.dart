fde901fc126fffbcbdfc66a25b5db7254d31684f
diikoyy <nguyenvuduykhoi2001@gmail.com>
2022-05-31 14:26:42 +0700

    Almost complete friend's chat room (minor bug)

--- lib/views/friend/chat_room_view.dart
+++ lib/views/friend/chat_room_view.dart
@@ -21,6 +21,7 @@ class ChatRoomView extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatRoomView> {
+  bool isSameUserLastMessage = false;
   late AuthUser user;
   late String chatroomUID;
   String? myName, myProfilePic, myUserName, myEmail;
@@ -34,58 +35,73 @@ class _ChatScreenState extends State<ChatRoomView> {
     // asyncMethod();
   }
 
-  Widget avatarAndTime(String senderUID, Timestamp timestamp)
-  {
+  Widget avatarAndTime(
+      String senderUID, Timestamp timestamp, bool isSameUserLastMessage) {
     DateTime myDateTime = timestamp.toDate();
-    var timestampString = myDateTime.hour.toString() + ':' + myDateTime.minute.toString();
-    return Row
-      (
+    var timestampString =
+        myDateTime.hour.toString() + ':' + myDateTime.minute.toString();
+    if (isSameUserLastMessage == true) {
+      return const SizedBox(
+        width: 0,
+        height: 0,
+      );
+    }
+    return Wrap(
+      direction: Axis.horizontal,
+      textDirection:
+          (senderUID == user.uid) ? TextDirection.rtl : TextDirection.ltr,
       children: [
         CircleAvatar(
-          backgroundImage:
-          NetworkImage((senderUID == user.uid)?
-          user.photoURL! : widget.selectedUser.photoURL!,
+          backgroundImage: NetworkImage(
+            (senderUID == user.uid)
+                ? user.photoURL!
+                : widget.selectedUser.photoURL!,
           ),
           radius: 15,
         ),
-
-        const SizedBox(width: 1,),
-
+        const SizedBox(
+          width: 1,
+        ),
         Text(timestampString),
-
       ],
     );
   }
 
-
-  Widget chatMessageTile(String message, String senderUID, Timestamp timestamp) {
+  Widget chatMessageTile(String message, String senderUID, Timestamp timestamp,
+      bool isSameUserLastMessage) {
     return Row(
-      mainAxisAlignment:
-      (senderUID == user.uid) ? MainAxisAlignment.end : MainAxisAlignment.start,
+      mainAxisAlignment: (senderUID == user.uid)
+          ? MainAxisAlignment.end
+          : MainAxisAlignment.start,
       children: [
         Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  bottomRight: (senderUID == user.uid) ? Radius.circular(0) : Radius.circular(24),
-                  topRight: Radius.circular(24),
-                  bottomLeft: (senderUID == user.uid) ? Radius.circular(24) : Radius.circular(0),
+            crossAxisAlignment: (senderUID == user.uid)
+                ? CrossAxisAlignment.end
+                : CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    bottomRight: (senderUID == user.uid)
+                        ? Radius.circular(0)
+                        : Radius.circular(24),
+                    topRight: Radius.circular(24),
+                    bottomLeft: (senderUID == user.uid)
+                        ? Radius.circular(24)
+                        : Radius.circular(0),
+                  ),
+                  color: Colors.blue,
                 ),
-                color: Colors.blue,
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  message,
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              padding: EdgeInsets.all(16),
-              child: Text(
-                message,
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-
-            avatarAndTime(senderUID, timestamp),
-          ]
-        ),
+              avatarAndTime(senderUID, timestamp, isSameUserLastMessage)
+            ]),
       ],
     );
   }
@@ -107,16 +123,14 @@ class _ChatScreenState extends State<ChatRoomView> {
             return Scaffold(
               appBar: AppBar(
                 title: Row(
-                  children: <Widget>[
+                  children: [
                     CircleAvatar(
-                      backgroundImage: NetworkImage(widget.selectedUser.photoURL!),
+                      backgroundImage:
+                          NetworkImage(widget.selectedUser.photoURL!),
                     ),
-
-                    const SizedBox
-                      (
+                    const SizedBox(
                       width: 20,
                     ),
-
                     Text(widget.selectedUser.displayName!),
                   ],
                 ),
@@ -128,60 +142,70 @@ class _ChatScreenState extends State<ChatRoomView> {
                     StreamBuilder<QuerySnapshot>(
                       stream: messageStream,
                       builder: (context, snapshot) {
+                        String? lastMessageUserUID = '';
                         //return Container();
-                        if(snapshot.connectionState == ConnectionState.waiting)
-                          {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                        if (snapshot.hasData == false || snapshot.data!.docs.isEmpty) {
-                          return Center(
-                            child: Text('No messages yet, chat to ${widget.selectedUser.displayName}!'),
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
                           );
                         }
-                            messageLength = snapshot.data?.docs.length;
-                            return ListView.builder(
-                                padding: const EdgeInsets.only(bottom: 80, top: 16),
-                                itemCount: snapshot.data!.docs.length,
-                                reverse: true,
-                                itemBuilder: (context, index) {
-                                  var messageInfo = snapshot.data!.docs.elementAt(index).data() as Map<String, dynamic>;
-                                  var message = messageInfo['message'];
-                                  var senderUID = messageInfo['senderUID'];
-                                  Timestamp timestamp = messageInfo['ts'];
-                                  return chatMessageTile(message, senderUID, timestamp);
-                                  // DocumentSnapshot ds = snapshot.data!.docs[index];
-                                  // return chatMessageTile(
-                                  //     ds['message'], user.uid == ds['senderUid']);
-                                }
-                            );
-
+                        if (snapshot.hasData == false ||
+                            snapshot.data!.docs.isEmpty) {
+                          return Center(
+                            child: Text(
+                                'No messages yet, chat to ${widget.selectedUser.displayName}!'),
+                          );
+                        }
+                        messageLength = snapshot.data?.docs.length;
+                        return ListView.builder(
+                            padding: const EdgeInsets.only(bottom: 80, top: 16),
+                            itemCount: snapshot.data!.docs.length,
+                            reverse: true,
+                            itemBuilder: (context, index) {
+                              var messageInfo = snapshot.data!.docs
+                                  .elementAt(index)
+                                  .data() as Map<String, dynamic>;
+                              var message = messageInfo['message'];
+                              var senderUID = messageInfo['senderUID'];
+                              Timestamp timestamp = messageInfo['ts'];
+                              if (senderUID == lastMessageUserUID) {
+                                isSameUserLastMessage = true;
+                                lastMessageUserUID = senderUID;
+                              } else {
+                                isSameUserLastMessage = false;
+                                lastMessageUserUID = senderUID;
+                              }
+                              return chatMessageTile(message, senderUID,
+                                  timestamp, isSameUserLastMessage);
+                              // DocumentSnapshot ds = snapshot.data!.docs[index];
+                              // return chatMessageTile(
+                              //     ds['message'], user.uid == ds['senderUid']);
+                            });
                       },
-                    )
-                    ,
+                    ),
                     Container(
                       alignment: Alignment.bottomCenter,
                       child: Container(
                         color: Colors.black.withOpacity(0.8),
                         padding:
-                        EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                         child: Row(
                           children: [
                             Expanded(
                                 child: TextField(
-                                  controller: messageTextEditingController,
-                                  onChanged: (value) {},
-                                  //border: InputBorder.none to get rid of underline things
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Type a message!",
-                                    hintStyle: TextStyle(
-                                        fontWeight: FontWeight.w400,
-                                        color: Colors.white.withOpacity(0.6)),
-                                  ),
-                                  style: TextStyle(color: Colors.white),
-                                )),
+                              controller: messageTextEditingController,
+                              onChanged: (value) {},
+                              //border: InputBorder.none to get rid of underline things
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Type a message!",
+                                hintStyle: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.white.withOpacity(0.6)),
+                              ),
+                              style: TextStyle(color: Colors.white),
+                            )),
                             GestureDetector(
                               onTap: () {
                                 Firestore.setMessage(
