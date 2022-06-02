ee483fc8c87bda9c1a9480e7bc1cc0a303b10b35
tuhuynh99 <hctu.study@gmail.com>
2022-06-02 12:21:50 +0700

    Beta change, not yet refine because my keyboard runs out of battery

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -10,8 +10,9 @@ import 'package:etoet/views/settingUI_lib/src/settings_tile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // import 'package:settings_ui/settings_ui.dart';
+import '../../services/auth/auth_user.dart';
 
-enum MenuAction { signOut }
+// enum MenuAction { signOut }
 
 class SettingsView extends StatefulWidget {
   @override
@@ -24,14 +25,60 @@ class SettingsView extends StatefulWidget {
 class _SettingsViewState extends State<SettingsView> {
   bool notificationsEnabled = true;
   double _receivedRange = 5.0;
-  final String _username = 'Doraemon';
+  // String _username = 'Doraemon';
+  late AuthUser? user;
+  // String _userName = user!.displayName ?? '';
+  String? photoURL;
+
+
 
   @override
   Widget build(BuildContext context) {
-    var user = context.watch<AuthUser?>();
+    user = context.watch<AuthUser?>();
+    photoURL = user!.photoURL;
+    // String userName = user!.displayName ?? '';
+    // String get username => null;
     return Scaffold(
       appBar: AppBar(title: const Text('Settings UI')),
       body: buildSettingsList(),
+    );
+  }
+
+  void showAlertDialog(BuildContext context) {
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("No"),
+      onPressed:  () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Yes"),
+        onPressed: () async {
+        await FirebaseAuth.instance.signOut();
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          loginRoute,
+          (_) => false,
+        );
+      }
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Logout"),
+      content: Text("Are you sure you want to logout?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 
@@ -42,18 +89,34 @@ class _SettingsViewState extends State<SettingsView> {
         SettingsSection(
           tiles: [
             SettingsTile(
-                titleWidget: CircleAvatar(
-                    radius: (50),
-                    backgroundColor: Colors.white,
-                    // backgroundImage: AssetImage('assets/Doraemon_character.png'),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(49),
-                      //image: const AssetImage('assets/images/google_logo.png')
-                      child: Image.asset('assets/images/Doraemon.png'),
-                    ))),
+                 titleWidget: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 3),
+              // image: ExactAssetImage(widget.user.img),
+                    image: DecorationImage(
+                        image: NetworkImage(user!.photoURL ??
+                    'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'),
+                    fit: BoxFit.cover,
+              ),
+            )),
+                //  CircleAvatar(
+                //     radius: (50),
+                //     backgroundColor: Colors.white,
+                //     // backgroundImage: AssetImage('assets/Doraemon_character.png'),
+                //     child: ClipRRect(
+                //       borderRadius: BorderRadius.circular(49),
+                //       //image: const AssetImage('assets/images/google_logo.png')
+                //       child:
+                //       Image.asset('assets/images/Doraemon.png'),
+                //     ))),
+            ),
             CustomTile(
               child: Text(
-                _username,
+                //username,
+                user!.displayName ?? '',
                 style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -74,13 +137,16 @@ class _SettingsViewState extends State<SettingsView> {
             SettingsTile(
                 title: 'Logout',
                 leading: const Icon(Icons.collections_bookmark),
-                onPressed: (_) async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    loginRoute,
-                    (_) => false,
-                  );
-                }),
+                onTap: () => showAlertDialog(context),
+                // onPressed: (_) async {
+                //   await FirebaseAuth.instance.signOut();
+                //   Navigator.of(context).pushNamedAndRemoveUntil(
+                //     loginRoute,
+                //     (_) => false,
+                //   );
+                // }
+
+            ),
             SettingsTile.switchTile(
               title: 'Push notifications',
               leading: const Icon(Icons.phonelink_lock),
@@ -90,6 +156,7 @@ class _SettingsViewState extends State<SettingsView> {
                   notificationsEnabled = value;
                 });
               },
+              switchActiveColor: Colors.orange,
             ),
             SettingsTile(
                 // title: 'Notification-received range',
@@ -110,7 +177,7 @@ class _SettingsViewState extends State<SettingsView> {
                 min: 5,
                 max: 20,
                 divisions: 3,
-                activeColor: Colors.blue,
+                activeColor: Colors.orange,
                 inactiveColor: Colors.grey,
                 value: _receivedRange,
                 onChanged: (var value) {
@@ -134,25 +201,25 @@ class _SettingsViewState extends State<SettingsView> {
                 title: 'Terms of use', leading: Icon(Icons.description)),
           ],
         ),
-        CustomSection(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 8),
-                child: Image.asset(
-                  'assets/images/settings.png',
-                  height: 50,
-                  width: 50,
-                  color: const Color(0xFF777777),
-                ),
-              ),
-              const Text(
-                'Version: Beta - Not yet integrate with other parts of the app',
-                style: TextStyle(color: Color(0xFF777777)),
-              ),
-            ],
-          ),
-        ),
+        // CustomSection(
+        //   child: Column(
+        //     children: [
+        //       Padding(
+        //         padding: const EdgeInsets.only(top: 15, bottom: 8),
+        //         child: Image.asset(
+        //           'assets/images/settings.png',
+        //           height: 50,
+        //           width: 50,
+        //           color: const Color(0xFF777777),
+        //         ),
+              //),
+              // const Text(
+              //   'Version: Beta - Not yet integrate with other parts of the app',
+              //   style: TextStyle(color: Color(0xFF777777)),
+              // ),
+            //],
+         // ),
+        //),
       ],
     );
   }
