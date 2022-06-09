ffd960a2b6a960015b6ee79c6da8fdb851b2f78b
PJ <triet403@gmail.com>
2022-06-09 21:46:07 +0700

    tested publicSignal + emulator data

--- functions/src/friend/userUpdateProfileNotification.ts
+++ functions/src/friend/userUpdateProfileNotification.ts
@@ -1,29 +1,29 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
 
-admin.initializeApp();
+// admin.initializeApp();
 
-const db = admin.firestore();
-const fcm = admin.messaging();
+// const db = admin.firestore();
+// const fcm = admin.messaging();
 
-export default async (
-    change: functions.Change<functions.firestore.QueryDocumentSnapshot>,
-    context: functions.EventContext,
-) => {
-  const userSnap = change.after.data();
-  const userDisplayName = String(userSnap.displayName);
-  const userEmail = String(userSnap.email);
-  const userPhotoUrl = String(userSnap.photoUrl);
-  const userUID = context.params.userUID;
-  const payload = {
-    data: {
-      type: "friendDataChanged",
-      displayName: userDisplayName,
-      email: userEmail,
-      photoUrl: userPhotoUrl,
-      uid: userUID,
-    },
-  };
+// export default async (
+//     change: functions.Change<functions.firestore.QueryDocumentSnapshot>,
+//     context: functions.EventContext,
+// ) => {
+//   const userSnap = change.after.data();
+//   const userDisplayName = String(userSnap.displayName);
+//   const userEmail = String(userSnap.email);
+//   const userPhotoUrl = String(userSnap.photoUrl);
+//   const userUID = context.params.userUID;
+//   const payload = {
+//     data: {
+//       type: "friendDataChanged",
+//       displayName: userDisplayName,
+//       email: userEmail,
+//       photoUrl: userPhotoUrl,
+//       uid: userUID,
+//     },
+//   };
 
-  // the next step is to get the list of all of the user's friends uid
-};
+//   // the next step is to get the list of all of the user's friends uid
+// };
