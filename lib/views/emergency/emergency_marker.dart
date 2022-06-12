43a90f9379a414e93d4216f8d5643e325485038e
Tam Thai <hoangtam3062002@gmail.com>
2022-06-12 10:40:20 +0700

    Show correct info on sos signal

--- lib/views/emergency/emergency_marker.dart
+++ lib/views/emergency/emergency_marker.dart
@@ -1,16 +1,15 @@
 import 'package:etoet/services/auth/user_info.dart' as etoet;
-import 'package:etoet/services/map/geocoding.dart';
+import 'package:etoet/services/map/marker/marker.dart';
 import 'package:etoet/services/map/osrm/routing.dart';
+import 'package:etoet/views/modal_bottom_sheet/confirm_box.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import 'package:etoet/services/map/marker/marker.dart';
 
 /// This class is used to create a marker for an emergency signal.
 class EmergencyMarker {
   Routing routing = Routing.getInstance();
   late etoet.UserInfo emergencyInfo;
-  late Future<BitmapDescriptor> friendIcon =
+  late Future<BitmapDescriptor> emergencyIcon =
       GoogleMapMarker.getIconFromUrl(emergencyInfo.photoURL!);
   late Set<Polyline> polylines;
   late BuildContext context;
@@ -29,46 +28,25 @@ class EmergencyMarker {
     required LatLng emergencyLatLng,
     required Function helpButtonPressed,
   }) async {
-    var location = await Geocoding.getAddress(emergencyLatLng);
+    var distance = await routing.getDistance(emergencyLatLng);
     return Marker(
       markerId: MarkerId(uid),
       position: emergencyLatLng,
-      icon: await friendIcon,
+      icon: await emergencyIcon,
       infoWindow: InfoWindow(
         title: emergencyInfo.displayName,
         snippet: 'Tap to see user\'s location',
       ),
       onTap: () {
-        showDialog(
+        showModalBottomSheet(
+            barrierColor: Colors.transparent,
             context: context,
             builder: (context) {
-              return AlertDialog(
-                title: const Text('User\'s location'),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(location),
-                    Text('Location description: $locationDescription'),
-                    Text('Situation detail: $situationDetail'),
-                  ],
-                ),
-                actions: <Widget>[
-                  ElevatedButton(
-                    child: const Text('Cancel'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ElevatedButton(
-                    child: const Text('Help'),
-                    onPressed: () {
-                      helpButtonPressed();
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
+              return Confirmbox(
+                locationDescription: locationDescription,
+                situationDetail: situationDetail,
+                distance: distance,
+                needHelpUser: emergencyInfo,
               );
             });
       },
--- lib/views/modal_bottom_sheet/confirm_box.dart
+++ lib/views/modal_bottom_sheet/confirm_box.dart
@@ -1,32 +1,33 @@
 import 'package:etoet/services/auth/user_info.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../services/auth/auth_user.dart';
 
 // just put here for the color reference, the tool is not good enought to generate the working code but it's ok for getting the color
 class FvColors {
-  static const Color screen1Background = Color.fromARGB(0,0,0,0);
-  static const Color container2Background = Color.fromARGB(255,199,9,9);
-  static const Color imageview3Background = Color.fromARGB(255,255,255,255);
-  static const Color textview4FontColor = Color.fromARGB(255,0,0,0);
-  static const Color button6Background = Color.fromARGB(255,132,2,2);
+  static const Color screen1Background = Color.fromARGB(0, 0, 0, 0);
+  static const Color container2Background = Color.fromARGB(255, 199, 9, 9);
+  static const Color imageview3Background = Color.fromARGB(255, 255, 255, 255);
+  static const Color textview4FontColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color button6Background = Color.fromARGB(255, 132, 2, 2);
 }
 
 class Confirmbox extends StatefulWidget {
   final double distance;
   final UserInfo needHelpUser;
-  // just for testing
-  static const mockUser = UserInfo(uid: 'CXIipI6L4xb2rfyDTvp7Dj0MIxs2');
+  final String locationDescription;
+  final String situationDetail;
 
-  Confirmbox({
+  const Confirmbox({
     Key? key,
-    this.distance = 5.0,
-    this.needHelpUser = mockUser,
+    required this.distance,
+    required this.needHelpUser,
+    required this.locationDescription,
+    required this.situationDetail,
   }) : super(key: key);
 
   @override
-  _ConfirmboxState createState() => _ConfirmboxState();
+  State<Confirmbox> createState() => _ConfirmboxState();
 }
 
 class _ConfirmboxState extends State<Confirmbox> {
@@ -35,157 +36,149 @@ class _ConfirmboxState extends State<Confirmbox> {
   String? displayName;
   String? email;
 
-
   @override
   Widget build(BuildContext context) {
-    double _distance = widget.distance;
-    UserInfo _needHelpUser = widget.needHelpUser;
-    needHelpUser = context.watch<AuthUser?>();
-    photoURL = needHelpUser?.photoURL;
-    displayName = needHelpUser?.displayName;
-    email = needHelpUser?.email;
+    var distance = widget.distance;
+    var needHelpUser = widget.needHelpUser;
+    photoURL = needHelpUser.photoURL;
+    displayName = needHelpUser.displayName;
+    email = needHelpUser.email;
 
     // double width = MediaQuery.of(context).size.width;
     // double height = MediaQuery.of(context).size.height;
     return Container(
+      alignment: Alignment.center,
       height: 300,
       //color: Colors.red,
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
           color: FvColors.container2Background,
           borderRadius: BorderRadius.only(
-              topRight: Radius.circular(20.0),
-              topLeft:  Radius.circular(20.0))),
+              topRight: Radius.circular(20.0), topLeft: Radius.circular(20.0))),
       child: Column(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: 5),
+            margin: const EdgeInsets.only(top: 30),
             height: 30,
-            child: Text('$_distance km away from you',
-                style: TextStyle(
+            child: Text('$distance km away from you',
+                style: const TextStyle(
                     fontSize: 20,
                     color: Colors.white,
                     fontWeight: FontWeight.bold)),
           ),
           Container(
-            height: 200,
+            height: 180,
             width: 370,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(20.0))
-          ),
-
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
             child: Column(
               children: [
                 Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  padding: EdgeInsets.only(left: 10, right: 10),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: NetworkImage(photoURL ??
-                                  'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'),
-                              fit: BoxFit.cover,
-                            ),
-                          )),
-
-                      Expanded(child: Container(
-                        alignment: Alignment.centerLeft,
-                        // margin: EdgeInsets.only(left: -5),
-                        child: Column(
-                          children: <Widget>[
+                    alignment: Alignment.center,
+                    height: 50,
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: NetworkImage(photoURL ??
+                                    'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            )),
+                        Expanded(
+                            child: Container(
+                          alignment: Alignment.centerLeft,
+                          // margin: EdgeInsets.only(left: -5),
+                          child: Column(children: <Widget>[
                             Padding(
                               padding: const EdgeInsets.only(top: 3.0),
                               child: Text('$displayName',
                                   textAlign: TextAlign.left,
-                                style: TextStyle(
+                                  style: const TextStyle(
                                     fontSize: 20,
                                     color: Colors.black,
                                     fontWeight: FontWeight.bold,
-                                    )),
+                                  )),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 15.0, top: 3.0),
+                              padding:
+                                  const EdgeInsets.only(left: 15.0, top: 3.0),
                               child: Text('$email',
                                   textAlign: TextAlign.left,
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold)),
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
                             ),
-                        ]),
-                      )),
-                      IconButton(
-                        icon: const Icon(Icons.message),
-                        onPressed: (){},
-                      )],
-                  )
-                ),
+                          ]),
+                        )),
+                        IconButton(
+                          icon: const Icon(Icons.message),
+                          onPressed: () {},
+                        )
+                      ],
+                    )),
                 Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.only(left: 10,right: 10),
-                  height: 30,
-                  child: Row(
-                    children: <Widget>[ Text('Emergency! - ', style: TextStyle(fontWeight: FontWeight.bold),),
-                      Text('Lost and Found', style: TextStyle(color: Colors.grey),)],
-                  )
-                ),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    height: 30,
+                    child: Row(
+                      children: const <Widget>[
+                        Text(
+                          'Emergency! - ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Lost and Found',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )),
                 Container(
-                  width: 360.0,
-                  height: 110.0,
-                  padding: EdgeInsets.only(left: 5,right: 5),
-                  color: Colors.transparent,
-                  alignment: Alignment.topCenter,
-
-                  child: SingleChildScrollView(
-    scrollDirection: Axis.vertical,//.horizontal
-    child: new Text(
-    "This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +
-    "and see the text. This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +"This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +
-    "and see the text. This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +"This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +
-    "and see the text. This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +"This SOS Notification Dummy Text is too long, didn't read, but you can scroll down " +
-    "and see the text. This SOS Notification Dummy Text is too long, didn't read, but you can scroll down ",
-    style: new TextStyle(
-    fontSize: 16.0, color: Colors.black,
-                )
-    ),))
+                    width: 360.0,
+                    padding: const EdgeInsets.only(left: 5, right: 5),
+                    color: Colors.transparent,
+                    alignment: Alignment.centerLeft,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical, //.horizontal
+                      child: Text('Situation: ${widget.situationDetail}'
+                          '\n'
+                          'Location: ${widget.locationDescription}'),
+                    ))
               ],
             ),
           ),
-        Container(
-            width: 300,
-            height: 39,
-            margin: EdgeInsets.only(top: 10),
-
-            child: TextButton(
-            child: Text('Confirm help ${displayName}',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: FvColors.imageview3Background,
-                    fontWeight: FontWeight.w700,
-                  )),
-              style: TextButton.styleFrom(
-                backgroundColor: FvColors.button6Background,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                  side: BorderSide(
-                    width: 0,
-                    color: Colors.transparent,
+          Container(
+              width: 300,
+              height: 39,
+              margin: const EdgeInsets.only(top: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: FvColors.button6Background,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    side: const BorderSide(
+                      width: 0,
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
-              ),
-              onPressed: () {},
-            )
-        )
+                onPressed: () {},
+                child: Text('Confirm help $displayName',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: FvColors.imageview3Background,
+                      fontWeight: FontWeight.w700,
+                    )),
+              )),
         ],
       ),
     );
   }
 }
-        
