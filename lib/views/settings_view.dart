c3a4c562080712fbda7f7451405332343c26f25d
truonghuy012345 <truonghuy012345@gmail.com>
2022-06-15 16:27:25 +0700

    Fix Image Blurring Problem

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -3,13 +3,9 @@ import 'package:etoet/services/auth/auth_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:etoet/views/settingUI_lib/src/custom_section.dart';
 import 'package:etoet/views/settingUI_lib/src/settings_list.dart';
 import 'package:etoet/views/settingUI_lib/src/settings_section.dart';
 import 'package:etoet/views/settingUI_lib/src/settings_tile.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../../services/auth/auth_user.dart';
 import 'package:etoet/services/database/firestore/firestore.dart';
 
@@ -22,7 +18,7 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
-  late bool notificationsEnabled ;
+  late bool notificationsEnabled;
   late double _receivedRange;
   late AuthUser? user;
   String? photoURL;
@@ -31,9 +27,9 @@ class _SettingsViewState extends State<SettingsView> {
   Widget build(BuildContext context) {
     user = context.watch<AuthUser?>();
     photoURL = user!.photoURL;
-    notificationsEnabled = user?.notificationsEnabled ?? true; // because of the legacy problem, the previous users may not have this field in Firestore
+    notificationsEnabled = user?.notificationsEnabled ??
+        true; // because of the legacy problem, the previous users may not have this field in Firestore
     _receivedRange = (user!.helpRange).toDouble();
-
 
     return Scaffold(
       appBar: AppBar(title: const Text('Settings UI')),
@@ -44,13 +40,13 @@ class _SettingsViewState extends State<SettingsView> {
   void showAlertDialog(BuildContext context) {
     // set up the buttons
     Widget cancelButton = TextButton(
-      child: Text("No"),
+      child: const Text('No'),
       onPressed: () {
         Navigator.of(context).pop();
       },
     );
     Widget continueButton = TextButton(
-        child: Text("Yes"),
+        child: const Text('Yes'),
         onPressed: () async {
           await FirebaseAuth.instance.signOut();
           Navigator.of(context).pushNamedAndRemoveUntil(
@@ -60,9 +56,9 @@ class _SettingsViewState extends State<SettingsView> {
         });
 
     // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: Text("Logout"),
-      content: Text("Are you sure you want to logout?"),
+    var alert = AlertDialog(
+      title: const Text('Logout'),
+      content: const Text('Are you sure you want to logout?'),
       actions: [
         cancelButton,
         continueButton,
@@ -72,7 +68,7 @@ class _SettingsViewState extends State<SettingsView> {
     // show the dialog
     showDialog(
       context: context,
-      builder: (BuildContext context) {
+      builder: (context) {
         return alert;
       },
     );
@@ -80,25 +76,31 @@ class _SettingsViewState extends State<SettingsView> {
 
   Widget buildSettingsList() {
     return SettingsList(
-      contentPadding: const EdgeInsets.only(top: 10, bottom:  10),
+      contentPadding: const EdgeInsets.only(top: 10, bottom: 10),
       sections: [
         SettingsSection(
           tiles: [
             SettingsTile(
-              titleWidget: Container(
-                width: 140.0,
-                height: 140.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(width: 3),
-                  // image: ExactAssetImage(widget.user.img),
-                  image: DecorationImage(
-                    image: NetworkImage(user!.photoURL ??
-                        'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'),
-                    fit: BoxFit.cover,
-                  ),
+              titleWidget: Stack(fit: StackFit.loose, children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        width: 140.0,
+                        height: 140.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 3),
+                          image: DecorationImage(
+                            image: NetworkImage(user!.photoURL ??
+                                'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        )),
+                  ],
                 ),
-              ),
+              ]),
             ),
             CustomTile(
               child: Container(
@@ -146,7 +148,7 @@ class _SettingsViewState extends State<SettingsView> {
                 titleWidget: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text('Notification-received range'),
+                    const Text('Notification-received range'),
                     Text('${_receivedRange.toStringAsFixed(1)} km'),
                   ],
                 ),
